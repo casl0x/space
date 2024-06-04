c44adf5f0a60728f4bc7b4ad7654da763515334a
Cassidy <cassidy.rouelle@gmail.com>
2024-06-04 16:27:39 +0200

    start destination + json

--- src/views/Destination.vue
+++ src/views/Destination.vue
@@ -3,7 +3,16 @@
 
 <template>
   <main>
-    Destination
+    <h2>01 pick your destination</h2>
+    <div><img src="" alt=""></div>
+    <div>
+      <h3></h3>
+      <p></p>
+      <div>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
   </main>
 </template>
 
